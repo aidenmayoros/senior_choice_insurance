18b37f69e1e8b6444b66214a7606bccfbeaeae92
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-06-15 08:16:30 -0700

    more tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	mode: 'jit',
+	content: [
+		'./src/**/*.{js,jsx,ts,tsx}', // Include all source files in src directory
+		'./public/index.html', // Include the main HTML file
+		'./src/**/*.html',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
