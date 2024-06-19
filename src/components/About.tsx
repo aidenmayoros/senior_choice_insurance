d2e677be0af4fdd1d16c3e3969acea778e5fee8e
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-06-18 22:17:02 -0700

    additions to services/about sections

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
+import waves from '../assets/images/waves.svg';
 
 const About: React.FC = () => {
 	return (
-		<div>
-			<div className=' m-8 text-center'>
+		<div className='relative w-full lg:min-h-72'>
+			<div
+				className='absolute inset-0 bg-cover bg-center opacity-5 lg:w-5/12'
+				style={{
+					backgroundImage: `url(${waves})`,
+				}}></div>
+			<div className='relative z-10 m-8 text-center'>
 				<h1 className='text-xl lg:text-2xl font-bold text-center'>
 					WHO WE ARE, AND WHAT WE DO
 				</h1>
-				<p className=' mt-4'>
+				<p className='mt-4 text-lg text-center mx-auto lg:w-6/12'>
 					At Senior Choice Insurance, we strive to make the insurance selection
 					process easy and convenient, putting our clients needs at the
-					forefront of everything we do.
+					forefront of everything we do. We believe insurance is more than just
+					an investment. It's a sign of your commitment to a quality life and
+					looking out for the people you care about. That's why we take the time
+					to get to know you and your needs, before offering solutions. As your
+					trusted partner, our objective is to provide the highest level of
+					customer service and education so that our clients feel confident and
+					secure with their insurance policies.
 				</p>
 			</div>
 		</div>
--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import ServicesImg from '../assets/images/services_hero.jpg';
 
 const Services: React.FC = () => {
 	return (
-		<div className='p-4'>
-			<h1 className='text-2xl'>Services Page</h1>
-			<p>Discover the services we offer.</p>
+		<div>
+			<h1 className='text-center text-3xl p-5'>
+				Discover our specialized insurance offerings:
+			</h1>
+			<div className='flex flex-col md:flex-row bg-[#8C8C8E] text-white'>
+				<div className='md:w-1/2 hidden md:block'>
+					<img
+						src={ServicesImg}
+						alt='Services'
+						className='w-full h-full object-cover'
+					/>
+				</div>
+				<div className='md:w-1/2 flex items-center'>
+					<ul className='mx-auto text-2xl list-disc'>
+						<li>Medicare Advantage</li>
+						<li>Medicare Supplement</li>
+						<li>Individual and Family Health Plans</li>
+						<li>Whole Life Insurance</li>
+						<li>Term Life Insurance</li>
+						<li>Final Expense Coverage</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
