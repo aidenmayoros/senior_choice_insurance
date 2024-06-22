b9f6978dec3e84f54d8eb5f1f7921accd88f86ca
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-06-21 17:57:51 -0700

    Front end save before creating backend code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,9 +21,7 @@ const Navbar: React.FC = () => {
 			<div className='sm:px-6'>
 				<div className='flex items-center justify-between h-16'>
 					<div className='flex items-center'>
-						<a href='#home'>
-							<img src={logo} alt='Company Logo' className='w-48 md:w-72' />
-						</a>
+						<img src={logo} alt='Company Logo' className='w-48 md:w-72' />
 						<div className='hidden md:block'>
 							<div className='ml-10 flex items-baseline space-x-4'>
 								<button
