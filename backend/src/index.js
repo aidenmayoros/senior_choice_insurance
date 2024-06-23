bcb8ab8af362461ab44d3adcd6ea0349b8268e67
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-06-22 18:37:10 -0700

    fixed pathname files

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../../frontend/dist')));
+app.use(express.static(path.join(__dirname, '../../dist')));
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html'));
+	res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 module.exports = app;
